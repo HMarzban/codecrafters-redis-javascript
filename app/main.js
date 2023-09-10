077d6d92bee8bc5824a9d742ec644e85522279b8
hossein <marzban98@gmail.com>
2023-09-10 20:20:09 +0330

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -4,8 +4,23 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-const server = net.createServer((connection) => {
+const server = net.createServer({ keepAlive: true }, (connection) => {
   // Handle connection
+  console.log("client connected");
+
+  connection.on("data", (data) => {
+    connection.write("+PONG\r\n");
+    if (data.toString().includes("exit")) {
+      connection.write("You will be disconnected\r\n");
+      connection.end();
+    }
+  });
+
+  connection.on("end", () => {
+    console.log("client disconnected");
+  });
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(6379, () => {
+  console.log("server is listening");
+});

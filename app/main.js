2e90fd7c57c8d817acd6cafd312e765e0861ef02
hossein <marzban98@gmail.com>
2023-09-10 19:45:28 +0330

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -4,8 +4,8 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-// const server = net.createServer((connection) => {
-//   // Handle connection
-// });
-//
-// server.listen(6379, "127.0.0.1");
+const server = net.createServer((connection) => {
+  // Handle connection
+});
+
+server.listen(6379, "127.0.0.1");

b857dcab2f6572b1f54e03bcb33982a13c7fcc53
hossein <marzban98@gmail.com>
2023-09-13 19:46:12 +0330

    pass 7nd stage

--- app/main.js
+++ app/main.js
@@ -4,30 +4,30 @@ const dataStore = new Map();
 
 const server = require("./worker")(dataStore);
 
-// if (cluster.isMaster) {
-//   // Code for the master process
-//   console.log(`Master ${process.pid} is running`);
+if (cluster.isMaster) {
+  // Code for the master process
+  console.log(`Master ${process.pid} is running`);
 
-//   // Fork workers
-//   const numWorkers = os.cpus().length;
-//   for (let i = 0; i < numWorkers; i++) {
-//     cluster.fork();
-//   }
+  // Fork workers
+  const numWorkers = os.cpus().length;
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
 
-//   // Handle worker events
-//   cluster.on("fork", (worker) => {
-//     console.log(`Worker ${worker.process.pid} is forked`);
-//   });
+  // Handle worker events
+  cluster.on("fork", (worker) => {
+    console.log(`Worker ${worker.process.pid} is forked`);
+  });
 
-//   cluster.on("exit", (worker, code, signal) => {
-//     console.log(
-//       `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
-//     );
-//     console.log("Forking a new worker...");
-//     cluster.fork();
-//   });
-// } else {
-server.listen(6379, () => {
-  console.log(`server is listening, PID: ${process.pid}`);
-});
-// }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+    );
+    console.log("Forking a new worker...");
+    cluster.fork();
+  });
+} else {
+  server.listen(6379, () => {
+    console.log(`server is listening, PID: ${process.pid}`);
+  });
+}

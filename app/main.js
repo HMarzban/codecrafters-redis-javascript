0c1f8a16116ec3d35f640eb1570dbe026a733579
hossein <marzban98@gmail.com>
2023-09-13 17:26:42 +0330

    pass 4nd stage

--- app/main.js
+++ app/main.js
@@ -1,26 +1,49 @@
 const net = require("net");
+const cluster = require("cluster");
+const os = require("os");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+if (cluster.isMaster) {
+  // Code for the master process
+  console.log(`Master ${process.pid} is running`);
 
-// Uncomment this block to pass the first stage
-const server = net.createServer({ keepAlive: true }, (connection) => {
-  // Handle connection
-  console.log("client connected");
+  // Fork workers
+  const numWorkers = os.cpus().length;
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
 
-  connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
-    if (data.toString().includes("exit")) {
-      connection.write("You will be disconnected\r\n");
-      connection.end();
-    }
+  // Handle worker events
+  cluster.on("fork", (worker) => {
+    console.log(`Worker ${worker.process.pid} is forked`);
   });
 
-  connection.on("end", () => {
-    console.log("client disconnected");
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+    );
+    console.log("Forking a new worker...");
+    cluster.fork();
   });
-});
+} else {
+  // Code for worker processes
+  const server = net.createServer({ keepAlive: true }, (connection) => {
+    // Handle connection
+    console.log("client connected, PID:", process.pid);
 
-server.listen(6379, () => {
-  console.log("server is listening");
-});
+    connection.on("data", (data) => {
+      connection.write("+PONG\r\n");
+      if (data.toString().includes("exit")) {
+        connection.write("You will be disconnected\r\n");
+        connection.end();
+      }
+    });
+
+    connection.on("end", () => {
+      console.log("client disconnected, PID:", process.pid);
+    });
+  });
+
+  server.listen(6379, () => {
+    console.log(`server is listening, PID: ${process.pid}`);
+  });
+}

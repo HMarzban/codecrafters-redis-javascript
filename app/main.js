a473a5770e71157bfd2f48bb585dd907722c64ef
hossein <marzban98@gmail.com>
2023-09-14 18:00:12 +0330

    pass 7nd stage

--- app/main.js
+++ app/main.js
@@ -13,16 +13,19 @@ if (cluster.isMaster) {
   cluster.on("message", (worker, message) => {
     if (message.command === "set") {
       dataStore.set(message.key, message.value);
+      console.log("set");
       worker.send({ status: "ok" });
     } else if (message.command === "has") {
       const hasKey = dataStore.has(message.key);
       worker.send({ exists: hasKey });
     } else if (message.command === "get") {
+      console.log("get");
       const value = dataStore.get(message.key);
       worker.send({ value: value });
     } else if (message.command === "delete") {
       if (dataStore.has(message.key)) {
         dataStore.delete(message.key);
+        console.log("delete");
         worker.send({ status: "ok" });
       } else {
         worker.send({ status: "not_found" });

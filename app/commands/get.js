9cd6ad59169d321014760eab0d8282e0757d7186
hossein <marzban98@gmail.com>
2023-09-14 17:52:37 +0330

    pass 7nd stage

--- app/commands/get.js
+++ app/commands/get.js
@@ -3,6 +3,11 @@ const db = require("../db");
 const GET = async (connection, data) => {
   const key = data.at(0);
   const result = await db.get(key);
+  console.log({
+    key,
+    result,
+    data,
+  });
   if (result) connection.write(`+${result.value}\r\n`);
   else connection.write("$-1\r\n");
 };
